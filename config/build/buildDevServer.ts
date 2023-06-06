98c9d54cf20c0e0653ab626b7712fd9bc5639782
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2023-06-06 15:44:10 +0300

    Роутинг

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,5 +6,7 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     port: options.port,
     // автоматически открывать страницу с приложением в браузере
     open: true,
+    // Проксировать запросы через index страницу (главную). Чтобы при переходе по прямой ссылке на любую страницу Роутер "подхватывал" адрес и рендерил то, что надо
+    historyApiFallback: true,
   }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { Counter } from './components/Counter'
+import { Link } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import AboutPage from './pages/AboutPage/AboutPage'
+import MainPage from './pages/MainPage/MainPage'
 
 const App = () => {
   return (
     <div className="app">
-      Hello world!
-      <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+
+      <Routes>
+        <Route path={'/about'} element={<AboutPage />} />
+        <Route path={'/'} element={<MainPage />} />
+      </Routes>
     </div>
   )
 }
