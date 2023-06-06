321e09fd2746e302c4a9db03aa0fdf18607eaf39
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2023-06-06 17:15:49 +0300

    Организация стилей. Добавляем темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { Suspense } from 'react'
+import { Suspense, useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Route, Routes } from 'react-router-dom'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
 import { MainPageAsync } from './pages/MainPage/MainPage.async'
+import './styles/index.scss'
+import { Theme, ThemeContext } from './theme/ThemeContext'
+import { useTheme } from './theme/useTheme'
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
 
